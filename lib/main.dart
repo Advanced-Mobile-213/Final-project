4f5a4468150509d8dd837290bafa4ad8eaa98bf5
21120569 <21120569@student.hcmus.edu.vn>
2024-10-22 21:11:33 +0700

    [Tien]#2: Create Navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
-import 'package:chatbot_agents/screens/login/login_screen.dart';
+import 'package:chatbot_agents/screens/login/login.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     initialRoute: "/login",
     routes: {
-      '/login' : (context) => LoginScreen(),
+      '/login' : (context) => Login(),
     },
 
   ));
