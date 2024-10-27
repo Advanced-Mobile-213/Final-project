a3d3e7ef9741ede8d3cfd1b6d95967adf8ad6a92
tuan.truong <truonganhtuan3092@gmail.com>
2024-10-27 11:39:14 +0700

    [Tuan]: fix order of messages

--- lib/views/ai_bot/ai_bot.dart
+++ lib/views/ai_bot/ai_bot.dart
@@ -2,5 +2,3 @@ export 'ai_bot_create_view.dart';
 export 'ai_bot_detail_view.dart';
 export 'ai_bot_publish_view.dart';
 export 'ai_bot_view.dart';
-export 'chat_history_view.dart';
-export 'chat_thread_view.dart';
