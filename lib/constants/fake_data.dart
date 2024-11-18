faeda6c55e11a2061504c585edba92d9829c6b51
Hoàng Quốc <hoangquoc2106@gmail.com>
2024-11-18 11:37:54 +0700

    Adding Fake Knowledges data

--- lib/constants/fake_data.dart
+++ lib/constants/fake_data.dart
@@ -1,3 +1,5 @@
+import 'package:chatbot_agents/models/knowledge.dart';
+
 class FakeData {
   static List<String> aiBots = [
     'Gemini',
@@ -11,10 +13,102 @@ class FakeData {
     'Promp2',
     'Promp3',
   ];
-  static List<String> knowledges = [
-    'Knowledge1',
-    'Knowledge2',
-    'Knowledge3',
-    'Knowledge4',
+  static List<Knowledge> knowledge = [
+    Knowledge(
+      createdAt: DateTime(2022, 1, 30),
+      userId: '1',
+      knowledgeName: 'Knowledge 1',
+      description: 'Description 1',
+    ),
+    Knowledge(
+      createdAt: DateTime(2023, 1, 30),
+      userId: '1',
+      knowledgeName: 'Knowledge 2',
+      description: 'Description 2',
+    ),
+    Knowledge(
+      createdAt: DateTime(2024, 1, 30),
+      userId: '1',
+      knowledgeName: 'Knowledge 3',
+      description: 'Description 3',
+    ),
+    Knowledge(
+      createdAt: DateTime(2021, 1, 30),
+      userId: '1',
+      knowledgeName: 'Knowledge 4',
+      description: 'Description 4',
+    ),
+    Knowledge(
+      createdAt: DateTime(2022, 1, 30),
+      userId: '1',
+      knowledgeName: 'Knowledge 5',
+      description: 'Description 5',
+    ),
+    Knowledge(
+      createdAt: DateTime(2023, 1, 30),
+      userId: '1',
+      knowledgeName: 'Knowledge 6',
+      description: 'Description 6',
+    ),
+    Knowledge(
+      createdAt: DateTime(2024, 1, 30),
+      userId: '1',
+      knowledgeName: 'Knowledge 7',
+      description: 'Description 7',
+    ),
+    Knowledge(
+      createdAt: DateTime(2021, 1, 30),
+      userId: '1',
+      knowledgeName: 'Knowledge 8',
+      description: 'Description 8',
+    ),
+    Knowledge(
+      createdAt: DateTime(2022, 1, 30),
+      userId: '1',
+      knowledgeName: 'Knowledge 9',
+      description: 'Description 9',
+    ),
+    Knowledge(
+      createdAt: DateTime(2023, 1, 30),
+      userId: '1',
+      knowledgeName: 'Knowledge 10',
+      description: 'Description 10',
+    ),
+    Knowledge(
+      createdAt: DateTime(2024, 1, 30),
+      userId: '1',
+      knowledgeName: 'Knowledge 11',
+      description: 'Description 11',
+    ),
+    Knowledge(
+      createdAt: DateTime(2021, 1, 30),
+      userId: '1',
+      knowledgeName: 'Knowledge 12',
+      description: 'Description 12',
+    ),
+    Knowledge(
+      createdAt: DateTime(2022, 1, 30),
+      userId: '1',
+      knowledgeName: 'Knowledge 13',
+      description: 'Description 13',
+    ),
+    Knowledge(
+      createdAt: DateTime(2023, 1, 30),
+      userId: '1',
+      knowledgeName: 'Knowledge 14',
+      description: 'Description 14',
+    ),
+    Knowledge(
+      createdAt: DateTime(2024, 1, 30),
+      userId: '1',
+      knowledgeName: 'Knowledge 15',
+      description: 'Description 15',
+    ),
+    Knowledge(
+      createdAt: DateTime(2021, 1, 30),
+      userId: '1',
+      knowledgeName: 'Knowledge 16',
+      description: 'Description 16',
+    ),
   ];
 }
