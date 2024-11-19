a0d2b16e4ec9c05cfa171d8d877e0f831ea1c10c
Hoàng Quốc <hoangquoc2106@gmail.com>
2024-11-19 17:40:29 +0700

    Add AIBot model

---
+++ lib/models/ai_bot.dart
@@ -0,0 +1,42 @@
+class AiBot {
+  final DateTime createdAt;
+  final DateTime? updatedAt;
+  final String? createdBy;
+  final String? updatedBy;
+  final String id;
+  final String assistantName;
+  final String openAiAssistantId;
+  final String? instructions;
+  final String? description;
+  final String? openAiThreadIdPlay;
+
+  AiBot({
+    required this.createdAt,
+    this.updatedAt,
+    this.createdBy,
+    this.updatedBy,
+    required this.id,
+    required this.assistantName,
+    required this.openAiAssistantId,
+    this.instructions,
+    this.description,
+    this.openAiThreadIdPlay,
+  });
+
+  factory AiBot.fromJson(Map<String, dynamic> json) {
+    return AiBot(
+      createdAt: DateTime.parse(json['createdAt']),
+      updatedAt: json['updatedAt'] != null
+          ? DateTime.parse(json['updatedAt'])
+          : null,
+      createdBy: json['createdBy'],
+      updatedBy: json['updatedBy'],
+      id: json['_id'],
+      assistantName: json['assistantName'],
+      openAiAssistantId: json['openAiAssistantId'],
+      instructions: json['instructions'],
+      description: json['description'],
+      openAiThreadIdPlay: json['openAiThreadIdPlay'],
+    );
+  }
+}
--- lib/models/models.dart
+++ lib/models/models.dart
@@ -3,3 +3,4 @@ export 'knowledge.dart';
 export 'knowledge_unit.dart';
 export 'prompt.dart';
 export 'data_source.dart';
+export 'ai_bot.dart';
